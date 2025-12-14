ef5d634ba8785957187af1d090dac31ee6198e12
enigma-137 <onojaemmanuel953@gmail.com>
2025-12-14 06:06:10 +0100

    Icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,23 +10,6 @@ const _geistMono = Geist_Mono({ subsets: ["latin"] })
 export const metadata: Metadata = {
   title: "Boyfriend Application Form",
   description: "Think you have what it takes? Apply here!",
-  icons: {
-    icon: [
-      {
-        url: "/icon-light-32x32.png",
-        media: "(prefers-color-scheme: light)",
-      },
-      {
-        url: "/icon-dark-32x32.png",
-        media: "(prefers-color-scheme: dark)",
-      },
-      {
-        url: "/icon.svg",
-        type: "image/svg+xml",
-      },
-    ],
-    apple: "/apple-icon.png",
-  },
 }
 
 export default function RootLayout({
